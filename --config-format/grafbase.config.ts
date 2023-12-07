60968388f2611fb8180ef60b617555b77897c86e
Siddhant <siddhantgupta200103@gmail.com>
2023-12-07 18:15:18 +0530

    schema code

--- --config-format/grafbase.config.ts
+++ --config-format/grafbase.config.ts
@@ -1,29 +1,59 @@
-import { g, auth, config } from '@grafbase/sdk'
+import { g, auth, config } from '@grafbase/sdk';
 
-const User= g.model('User' , {
-  name: g.string().length({min: 2 , max: 20}),
-  email:g.string().unique(),
-  avatarUrl: g.url(),
-  description: g.string().optional(),
-  githubUrl:g.url().optional(),
-  linkedInUrl: g.url().optional(),
-  projects: g.relation(() => Project).list().optional(),
-})
-
-const Project= g.model("Project", {
-  title: g.string().length({min: 3}),
+const Project = g.model("Project", {
+  title: g.string().length({ min: 3 }),
   description: g.string(),
-  image:g.url(),
-  liveSiteUrl:g.url(),
-  githubUrl:g.url(),
+  image: g.url(),
+  liveSiteUrl: g.url(),
+  githubUrl: g.url(),
   category: g.string().search(),
   createdBy: g.relation(() => User),
-})
+});
 
-
+const User = g.model('User', {
+  name: g.string().length({ min: 2, max: 20 }),
+  email: g.string().unique(),
+  avatarUrl: g.url(),
+  description: g.string().optional(),
+  githubUrl: g.url().optional(),
+  linkedInUrl: g.url().optional(),
+  projects: g.relation(() => Project).list().optional(),
+});
 
 export default config({
-  schema: g
+  schema: g,
+});
+
+
+
+
+
+// import { g, auth, config } from '@grafbase/sdk'
+
+// const User= g.model('User' , {
+//   name: g.string().length({min: 2 , max: 20}),
+//   email:g.string().unique(),
+//   avatarUrl: g.url(),
+//   description: g.string().optional(),
+//   githubUrl:g.url().optional(),
+//   linkedInUrl: g.url().optional(),
+//   projects: g.relation(() => Project).list().optional(),
+// })
+
+// const Project= g.model("Project", {
+//   title: g.string().length({min: 3}),
+//   description: g.string(),
+//   image:g.url(),
+//   liveSiteUrl:g.url(),
+//   githubUrl:g.url(),
+//   category: g.string().search(),
+//   createdBy: g.relation(() => User),
+// })
+
+
+
+// export default config({
+//   schema: g
  
    
-})
+// })
